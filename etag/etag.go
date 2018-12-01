cf50eaa75d73f3c14da241b776d756faba4789df
nickhstr <nickhstr@gmail.com>
2018-11-30 22:43:57 -0500

    refactor: rename getHash

--- etag/etag.go
+++ etag/etag.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 )
 
-func getHash(p []byte) string {
+func hash(p []byte) string {
 	return fmt.Sprintf("%x", sha1.Sum(p))
 }
 
 // Generate an etag for given byte slice. Can be set as weak with second boolean parameter.
 func Generate(p []byte, weak bool) string {
-	tag := fmt.Sprintf("\"%d-%s\"", len(p), getHash(p))
+	tag := fmt.Sprintf("\"%d-%s\"", len(p), hash(p))
 	if weak {
 		tag = "W/" + tag
 	}
