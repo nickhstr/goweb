2db72ffa301aa5599acb8d2c92847946aee04795
nickhstr <nickhstr@gmail.com>
2018-12-03 16:01:16 -0500

    feat: add etag verification

--- middleware/etag.go
+++ middleware/etag.go
@@ -6,24 +6,32 @@ import (
 	"github.com/nickhstr/goweb/etag"
 )
 
-type etagWriter struct {
+// EtagWriter provides a ResponseWriter which holds info needed for verifying etags.
+type EtagWriter struct {
 	http.ResponseWriter
-	status int
+	status     int
+	clientEtag string
 }
 
 // WriteHeader must be defined to override the ResponseWriter's WriteHeader.
 // This is needed to set the etag header in etagWriter.Write, as ResponseWriter's
 // WriteHeader prevents setting headers after it is called.
-func (e *etagWriter) WriteHeader(status int) {
+func (e *EtagWriter) WriteHeader(status int) {
 	e.status = status
 }
 
-func (e *etagWriter) Write(p []byte) (int, error) {
+func (e *EtagWriter) Write(p []byte) (int, error) {
 	et := etag.Generate(p, false)
 	e.ResponseWriter.Header().Set("ETag", et)
+
+	if e.clientEtag == et {
+		// Set status to not modified, and write empty body
+		e.ResponseWriter.WriteHeader(http.StatusNotModified)
+		return e.ResponseWriter.Write([]byte{})
+	}
+
 	// Make sure to call ResponseWriter's WriteHeader, with stored status
 	e.ResponseWriter.WriteHeader(e.status)
-
 	return e.ResponseWriter.Write(p)
 }
 
@@ -31,7 +39,10 @@ func (e *etagWriter) Write(p []byte) (int, error) {
 // bodies.
 func Etag(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		e := &etagWriter{ResponseWriter: w}
+		e := &EtagWriter{
+			ResponseWriter: w,
+			clientEtag:     r.Header.Get("If-None-Match"),
+		}
 
 		handler.ServeHTTP(e, r)
 	})
