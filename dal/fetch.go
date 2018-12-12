e50518caff59882b058d96ee729bc2d56dfd846c
nickhstr <nickhstr@gmail.com>
2018-12-11 21:06:32 -0500

    refactor: move cache package outside of dal package dir

--- dal/fetch.go
+++ dal/fetch.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/nickhstr/goweb/dal/cache" // nolint: gotype
+	"github.com/nickhstr/goweb/cache" // nolint: gotype
 	"github.com/nickhstr/goweb/logger"
 )
 
