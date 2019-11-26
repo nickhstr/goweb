34da3547681770b6c63cf62e88dbc149112cf6ce
nickhstr <nickhstr@gmail.com>
2019-11-26 13:48:25 -0800

    feat: add default secure options

--- middleware/secure.go
+++ middleware/secure.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"reflect"
 
 	"github.com/nickhstr/goweb/env"
 	"github.com/unrolled/secure"
@@ -10,12 +11,22 @@ import (
 // Secure creates the security middleware
 func Secure(options secure.Options) Middleware {
 	return func(handler http.Handler) http.Handler {
+		// The performance impact of using reflect here is negligible, as it's only
+		// used once when getting the middleware handler
+		if reflect.DeepEqual(options, secure.Options{}) {
+			options = secure.Options{
+				BrowserXssFilter:        true,
+				ContentTypeNosniff:      true,
+				CustomFrameOptionsValue: "SAMEORIGIN",
+				STSSeconds:              180 * 24 * 60 * 60, // Default to 180 days
+				STSIncludeSubdomains:    true,
+			}
+		}
+
 		if !env.IsProd() {
 			options.IsDevelopment = true
 		}
 
-		secureMiddleware := secure.New(options)
-
-		return secureMiddleware.Handler(handler)
+		return secure.New(options).Handler(handler)
 	}
 }
