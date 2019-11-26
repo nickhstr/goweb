03e60da0b61e7a01f19849a77173c9baba6e8323
nickhstr <nickhstr@gmail.com>
2019-11-26 11:01:45 -0800

    refactor: remove charset directive

--- middleware/authentication.go
+++ middleware/authentication.go
@@ -80,7 +80,7 @@ func badAuthHandler(errMsg string) http.Handler {
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusUnauthorized)
 		_, _ = w.Write(errResponse)
 	})
